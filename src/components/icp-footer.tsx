7a328939429804d306ed7938c9db5693c6bea08c
Haoder <2357178581@qq.com>
2026-07-01 15:44:26 +0800

    chore: add ICP filing footer

---
+++ src/components/icp-footer.tsx
@@ -0,0 +1,14 @@
+export function IcpFooter() {
+  return (
+    <footer className="pb-2 text-center text-xs text-slate-400">
+      <a
+        className="transition hover:text-sky-600"
+        href="https://beian.miit.gov.cn/"
+        rel="noreferrer"
+        target="_blank"
+      >
+        辽ICP备2026013992号
+      </a>
+    </footer>
+  );
+}
